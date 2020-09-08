d63501ac1b169c62f1d41e927b2f69a5c13678ce
i-will-lie <i-will-lie@users.noreply.github.com>
2020-09-08 19:12:55 +1200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Action names correspond with cshtml files in /Home
+        // Default http method is 'get'
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet] // '[HttpGet]' attribute specifies http request method
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+             return RedirectToAction("Index"); // redirect to home page
+        }
+    }
+}
